12492de2adef6c7260e5ea8f3dea53a7a561f63d
rakibpaucse <rakibpaucse@gmail.com>
2023-05-09 03:32:22 +0600

    fin

--- middlewares/authMiddleWare.js
+++ middlewares/authMiddleWare.js
@@ -1,19 +1,20 @@
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
+const dotenv = require("dotenv").config()
 
 exports.isUnauth = (req , res , next) => {
 
     let bearerToken = req?.headers['authorization']?.split(' ')[1]
     if(bearerToken === null) return res.status(401).send({msg : 'Not Valid User!'})
 
-    // jwt.verify(bearerToken , process.env.ACCESS_TOKEN_SECRET , (err , user) => {
-    //     if(!err){
-    //         req.admin = true
-    //         next()
-    //     }else{
-    //         return res.status(403).send({msg : 'User Unauthorised'})
-    //     } 
+    jwt.verify(bearerToken , process.env.ACCESS_TOKEN_SECRET , (err , user) => {
+        if(!err){
+            req.admin = true
+            next()
+        }else{
+            return res.status(403).send({msg : 'User Unauthorised'})
+        } 
             
 
-    // })
+    })
 
 }
